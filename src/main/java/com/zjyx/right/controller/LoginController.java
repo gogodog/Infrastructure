240cbf2f024b87089ffa480f32d276ca157f094d
文甲东 <jiadong.wen@ly.com>
2018-12-14 11:13:08 +0800

    修改错误

--- src/main/java/com/zjyx/right/controller/LoginController.java
+++ src/main/java/com/zjyx/right/controller/LoginController.java
@@ -25,17 +25,17 @@ public class LoginController {
 	@RequestMapping("/login")
 	public BaseView select(String name, String pass){
 		if(StringUtils.isEmptyOrWhitespaceOnly(name)){
-			return BaseViewHelper.getBaseView(true, "10001", "用户名不能为空");
+			return BaseViewHelper.getFailBaseView("10001", "用户名不能为空");
 		}
 		if(StringUtils.isEmptyOrWhitespaceOnly(pass)){
-			return BaseViewHelper.getBaseView(true, "10002", "密码不能为空");
+			return BaseViewHelper.getFailBaseView("10002", "密码不能为空");
 		}
 		AdminUser user = adminUserService.getAdminUser(name, pass);
 		if(user == null || StringUtils.isEmptyOrWhitespaceOnly(user.getUsername())){
-			return BaseViewHelper.getBaseView(true, "20001", "密码错误");
+			return BaseViewHelper.getFailBaseView("20001", "密码错误");
 		}
 		if(UpDown_Status.DOWN == user.getStatus()){
-			return BaseViewHelper.getBaseView(true, "20002", "无效用户");
+			return BaseViewHelper.getFailBaseView("20002", "无效用户");
 		}
 		return BaseViewHelper.getSuccessBaseView();
 	}
